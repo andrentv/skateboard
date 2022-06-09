5f7d9ad84005114f747e78c778e2b06476ec21f2
andrentv <comercial@vaccaros.com.br>
2022-06-09 09:19:28 -0300

    consegui incluir codigo

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
 import MyComponent from './components/MyComponent.vue'
 
-
+const name = 'Andre'
 </script>
 
 <template>
   <h1 class="bg-blue-500">Testando Vue</h1>
-  <!-- <p>{{ name }}</p> -->
+  <p>{{ name }}</p>
   <MyComponent />
 </template>
 
